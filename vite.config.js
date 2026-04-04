25a67f277ff2b9dc197b568190f236cbe8fd3d8c
gilcleber <gilcleberlocutor@gmail.com>
2026-04-04 14:25:49 -0300

    fix: correct vite base path for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Se for build do Electron, usa base relativa './', senão usa '/sorteio-facil-web/' para GH Pages
-  base: process.env.ELECTRON_BUILD ? './' : '/sorteio-facil-web/',
+  // Se for build do Electron, usa base relativa './', senão usa a Raiz absoluta '/' para Vercel
+  base: process.env.ELECTRON_BUILD ? './' : '/',
 })
